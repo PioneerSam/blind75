5b5fcc46dcd2bc9e246b3415fe943fd42fc6cbbd
Sam Qiao <pioneerqxy@gmail.com>
2026-01-23 00:18:50 -0500

    python and cpp working

---
+++ python/optimize.py
@@ -0,0 +1,19 @@
+'''
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+'''
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        
+        
