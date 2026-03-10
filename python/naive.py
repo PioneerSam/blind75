63f9ed0bcc0cf0d81577669b027a089d63626cba
Xinyuan Sam Qiao <pioneerqxy@gmail.com>
2026-03-10 16:42:50 -0400

    solved python version

--- python/naive.py
+++ python/naive.py
@@ -1,45 +1,52 @@
 '''
 
-Given an integer array nums, return the length of the longest strictly increasing subsequence.
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
 
  
 
 Example 1:
 
-Input: nums = [10,9,2,5,3,7,101,18]
-Output: 4
-Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 Example 2:
 
-Input: nums = [0,1,0,3,2,3]
-Output: 4
-Example 3:
-
-Input: nums = [7,7,7,7,7,7,7]
-Output: 1
- 
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 '''
 
-
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-
-        if not nums:
-            return 0
+    def canJump(self, nums: List[int]) -> bool:
         
-        n = len(nums)
-        if n == 1:
-            return 1
-        
-        # dp approach
+        end_idx = len(nums)-1
+        # it is actually a DP....
+        memo = {}
 
-        dp = [1] * n
 
-        for i in range(0,n,1):
-            for j in range(0,i,1):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i],dp[j] + 1)
-        
+        def dfs(idx):
+            if idx >= end_idx:
+                return True
 
-        return max(dp)
+            if idx in memo:
+                return memo[idx]
+
+            maximum_jump = nums[idx]
+
+            if maximum_jump == 0 and idx < end_idx:
+                memo[idx] = False
+                return False
+
+            for jump in range(1,maximum_jump+1,1):
+                if dfs(idx + jump):
+                    memo[idx] = True
+                    return True
+            
+            memo[idx] = False
+            return False
+
+
+        return dfs(0)
--- python/optimize.py
+++ python/optimize.py
@@ -1,28 +1,38 @@
 '''
 
-Given an integer array nums, return the length of the longest strictly increasing subsequence.
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
 
  
 
 Example 1:
 
-Input: nums = [10,9,2,5,3,7,101,18]
-Output: 4
-Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 Example 2:
 
-Input: nums = [0,1,0,3,2,3]
-Output: 4
-Example 3:
-
-Input: nums = [7,7,7,7,7,7,7]
-Output: 1
- 
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 '''
 
 
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        # for the optimized O(nlongn) method
-        # you just need to use the tail + binary search
+    def canJump(self, nums: List[int]) -> bool:
+        # cannot use dp but only use greedy
+
+        furtherest = 0 # ....
+
+        for idx in range(0,len(nums),1):
+            if idx > furtherest:
+                return False
+            furtherest = max(furtherest, idx + nums[idx])
+        
+            if furtherest >= len(nums)-1:
+                return True
+            
+        return True
+
