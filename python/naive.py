8169d87a701d77dea516fedce1e7244dee2589a7
Xinyuan Sam Qiao <pioneerqxy@gmail.com>
2026-03-10 22:22:33 -0400

    python version somewhat solved

--- python/naive.py
+++ python/naive.py
@@ -1,54 +1,51 @@
 '''
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
 
- 
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
 
-Example 1:
-
-Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
-Example 2:
-
-Input: intervals = [[1,4],[4,5]]
-Output: [[1,5]]
-Explanation: Intervals [1,4] and [4,5] are considered overlapping.
-Example 3:
-
-Input: intervals = [[4,7],[1,4]]
-Output: [[1,7]]
-Explanation: Intervals [1,4] and [4,7] are considered overlapping.
+Return intervals after the insertion.
 
 '''
 
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # I think it is greedy again
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # we need to sort the intervals again haha
+        # intervals.sort(key = lambda x:x[0])
+        if not intervals:
+            return [newInterval]
+
+        new_left = newInterval[0]
+        new_right = newInterval[1]
+
         result = []
 
-        if not intervals:
-            return result
-        # I need to sort the intervals based on end 
-        intervals = sorted(intervals,key = lambda x:x[0])
-        current_min_l = intervals[0][0]
-        current_max_r = intervals[0][1]
+        overlapped = False
 
-        for interval in intervals[1::]:
-            print("current_min_l", current_min_l)
-            print("current max r",current_max_r)
+        for interval in intervals:
             current_l = interval[0]
             current_r = interval[1]
 
-            if current_l <= current_max_r:
-                current_min_l = min(current_min_l,current_l)
-                current_max_r = max(current_max_r,current_r)
-            else:
-                # we need to start another one
-                result.append([current_min_l,current_max_r])
-
-                current_min_l = current_l
-                current_max_r = current_r
+            # completely before
+            if current_r < new_left:
+                result.append([current_l,current_r])
+            # overlap
+            elif current_r >= new_left and current_l <= new_right:
+                new_left = min(current_l,new_left)
+                new_right = max(current_r,new_right)
+            # completely after
+            elif current_l > new_right:
+                if not overlapped:
+                    result.append([new_left,new_right])
+                    overlapped = True
+                result.append([current_l,current_r])
+            
+            print("new left",new_left)
+            print("new right,",new_right)
         
-        result.append([current_min_l,current_max_r])
+        if not overlapped:
+            result.append([new_left,new_right])
+
         return result
+
+
                        
