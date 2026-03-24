4b1661e1a9ca8933f74132aef21d074d2864df59
Xinyuan Sam Qiao <pioneerqxy@gmail.com>
2026-03-24 19:16:37 -0400

    top down and bottom down dp implemented in python

--- python/naive.py
+++ python/naive.py
@@ -1,51 +1,36 @@
 '''
-You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
 
-Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
-Return intervals after the insertion.
+You may assume that you have an infinite number of each kind of coin.
 
 '''
+import math
 
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # we need to sort the intervals again haha
-        # intervals.sort(key = lambda x:x[0])
-        if not intervals:
-            return [newInterval]
+    def coinChange(self, coins: List[int], amount: int) -> int:
+  
+        memo = {}
+        def dfs(remain):
 
-        new_left = newInterval[0]
-        new_right = newInterval[1]
+            best = math.inf
+            if remain == 0:
+                return 0
+            if remain in memo:
+                return memo[remain]
 
-        result = []
-
-        overlapped = False
-
-        for interval in intervals:
-            current_l = interval[0]
-            current_r = interval[1]
-
-            # completely before
-            if current_r < new_left:
-                result.append([current_l,current_r])
-            # overlap
-            elif current_r >= new_left and current_l <= new_right:
-                new_left = min(current_l,new_left)
-                new_right = max(current_r,new_right)
-            # completely after
-            elif current_l > new_right:
-                if not overlapped:
-                    result.append([new_left,new_right])
-                    overlapped = True
-                result.append([current_l,current_r])
+            for i in coins:
+                if remain - i >=0:
+                   best = min(dfs(remain-i)+1,best)
             
-            print("new left",new_left)
-            print("new right,",new_right)
-        
-        if not overlapped:
-            result.append([new_left,new_right])
+            memo[remain] = best
+            return best
 
-        return result
+        best = dfs(amount)
+        if best == math.inf:
+            best = -1
+        return best
 
 
                        
--- python/optimize.py
+++ python/optimize.py
@@ -1,38 +1,28 @@
 '''
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
 
-You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
-Return true if you can reach the last index, or false otherwise.
+You may assume that you have an infinite number of each kind of coin.
 
  
-
-Example 1:
-
-Input: nums = [2,3,1,1,4]
-Output: true
-Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
-Example 2:
-
-Input: nums = [3,2,1,0,4]
-Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
-
 '''
 
 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        # cannot use dp but only use greedy
+    def coinChange(self, coins: List[int], amount: int) -> int:
+  
+        dp = [float('inf')] * (amount+1)
 
-        furtherest = 0 # ....
+        dp[0] = 0
 
-        for idx in range(0,len(nums),1):
-            if idx > furtherest:
-                return False
-            furtherest = max(furtherest, idx + nums[idx])
-        
-            if furtherest >= len(nums)-1:
-                return True
-            
-        return True
+        for i in range(1,len(dp),1):
+            for coin_value in coins:
+                if i - coin_value == 0:
+                    dp[i] = min(dp[i],dp[i-coin_value] + 1)
 
+        if dp[amount] != float('inf'):
+            return dp[amount]
+        else:
+            return 0
+                
